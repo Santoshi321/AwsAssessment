50b2f8c6c9c31069281468c391ef80e85b3f5aba
Mahi <Santoshi321@gmail.com>
2019-07-02 16:35:49 -0400

    committing recap sql file

---
+++ recapfile.sql
@@ -0,0 +1,37 @@
+--using existing warehouse
+use demo_db;
+
+--creating new warehouse/database/table
+create or replace warehouse Recap_wh;
+
+create or replace database Recap_db;
+
+create or replace table emp_basic (
+  first_name string ,
+  last_name string ,
+  email string ,
+  streetaddress string ,
+  city string ,
+  start_date date
+  );
+
+--copying csv file contents to table 
+put file:///Users/sgandham/snowflake-tutorials/dataset/employees01*.csv @recap_db.public.%emp_basic;
+
+copy into emp_basic
+  from @%emp_basic
+  file_format = (type = csv field_optionally_enclosed_by='"')
+  pattern = '.*employees01.csv.gz'
+  on_error = 'skip_file';
+
+--creating function 
+create or replace function empl(first_n varchar)
+  returns table (email varchar, city varchar)
+  as
+  $$
+    select email,city from emp_basic where first_name = first_n
+  $$
+  ;
+   select * from table(empl('Avo'));
+   
+
